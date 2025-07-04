05c5d514744306c081df2d49d1e7a61eff0253e0
Radosław Wolnik <60506818+powolnik@users.noreply.github.com>
2025-07-04 06:14:57 +0000

    basic layout

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,14 +1,16 @@
 import { Container } from 'react-bootstrap'
 
 import Header from './components/Header'
-import Footer from './components/Footer';
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 
 function App() {
   return (
   <div>
     <Header />
-   
+    
+    <main><HomeScreen /></main>
     <Footer />
   </div>
   );
